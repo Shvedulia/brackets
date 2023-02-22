79ed80ce4e7c25d3887fb7b95e1148a70f048e68
Shvedulia <shvedalex16@gmail.com>
2023-02-22 23:16:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !==0 ) return false;
+  let option = bracketsConfig.map(conf => conf.join(''));
+  let preStr = str;
+  for(let i = 0; i < option.length; i++) {
+    preStr = preStr.replace(option[i], '');
+  }
+  if (preStr.length === 0) return true;
+  if (str !== preStr) return check(preStr, bracketsConfig);
+  return false
+};
